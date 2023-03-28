e720e802c0cc0065036b5296538bd86b7f859ec4
vaniakazeko <vania.kazeko.00@bk.ru>
2023-03-28 13:58:21 +0300

    add git

---
+++ store/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from products.views import index
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('', index, name='index'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
