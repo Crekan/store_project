3292fa4feaba594e8a6d148f5b8a741dfc73ad8e
vaniakazeko <vania.kazeko.00@bk.ru>
2023-03-28 18:27:07 +0300

    add app users

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from .views import products
+
+
+app_name = 'products'
+
 
 urlpatterns = [
+    path('', products, name='products'),
 ]
--- store/urls.py
+++ store/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from products.views import index, products
+from products.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', index, name='index'),
-    path('products/', products, name='products'),
+    path('products/', include('products.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
