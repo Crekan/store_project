69692e42634ba41ade5c7ab0919996ac7f24948e
vaniakazeko <vania.kazeko.00@bk.ru>
2023-03-28 14:41:22 +0300

    add models Product and ProductCategory

--- store/urls.py
+++ store/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from products.views import index
+from products.views import index, products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', index, name='index'),
+    path('products/', products, name='products'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
