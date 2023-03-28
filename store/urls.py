cd043ba658052771fb77b412fa87fc42197d2b73
vaniakazeko <vania.kazeko.00@bk.ru>
2023-03-28 20:36:37 +0300

    add login and register

--- store/urls.py
+++ store/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
 
     path('', index, name='index'),
     path('products/', include('products.urls')),
+    path('users/', include('users.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
